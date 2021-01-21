74fbeec1fefb65e9a42f7ac48aef25539489b89d
RohanPius <56528315+RohanPius@users.noreply.github.com>
2021-01-21 19:33:07 +0530

    Add files via upload

---
+++ New.py
@@ -0,0 +1,13 @@
+character_name = "Tom"
+character_age = "25"
+is_male = True
+print("There was once a man named " + character_name + ", ")
+print("he was " + character_age + " years old, ")
+character_name = "Mike"
+character_age = "21"
+print("he really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
+
+phrase = "Giraffe Academy"
+print(phrase.upper().isupper())
+     
