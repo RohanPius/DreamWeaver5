0b49b5198dd490f00863b6c97ed83d7936191461
RohanPius <56528315+RohanPius@users.noreply.github.com>
2021-01-21 19:44:55 +0530

    Interface_optional

---
+++ interface_optional.ts
@@ -0,0 +1,29 @@
+interface Student {
+
+    id: number,
+
+    name: string,
+
+    favoriteFood
+        ?
+        : string,
+
+    isAlumni
+        ?
+        : boolean
+
+}
+
+
+
+let ana: Student;
+
+
+
+ana = {
+
+    id : 25,
+
+    name : "ana"
+
+}
